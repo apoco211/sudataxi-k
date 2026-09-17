ad4baa6c4ce82cf4b53229ddc2178c3763cff38e
apoco211 <apoco211@gmail.com>
2026-09-18 00:49:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,11 @@
 
-const CACHE_NAME = 'sudataxi-k-v7-fixed';
-const urlsToCache = ['./','./index.html','./manifest.json'];
-
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>{
-      if(r) return r;
-      return fetch(e.request).then(res=>{
-        if(e.request.url.includes('google')) return res;
-        const clone=res.clone();
-        caches.open(CACHE_NAME).then(c=>c.put(e.request,clone));
-        return res;
-      }).catch(()=>{
-        if(e.request.destination==='document') return caches.match('./index.html');
-      });
-    })
-  );
+const CACHE='sudataxi-k-v10-return';
+const URLS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  if(e.request.url.includes('google')) return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const clone=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,clone)); return res;
+  }).catch(()=>e.request.destination==='document'?caches.match('./index.html'):null)));
 });
